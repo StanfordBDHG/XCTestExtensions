3f9314c8c88b7cbfead968f7d27f8348daa26a74
Lukas Kollmer <hey@lukaskollmer.de>
2026-04-09 15:03:13 +0200

    try to fix tests

--- Tests/UITests/TestAppUITests/DeleteAppTests.swift
+++ Tests/UITests/TestAppUITests/DeleteAppTests.swift
@@ -35,15 +35,12 @@ final class DeleteAppTests: XCTestCase {
     }
     
     
-    @available(macOS, unavailable)
-    @available(watchOS, unavailable)
-    @available(visionOS, unavailable)
-    @available(tvOS, unavailable)
+    #if os(iOS)
     func testDeleteAndLaunchWithHealthKitSample() throws {
-        #if os(macOS) || os(watchOS) || os(visionOS) || os(tvOS)
-        throw XCTSkip("Not supported on this platform")
-        #endif
-
+        guard UIDevice.current.userInterfaceIdiom == .phone else {
+            throw XCTSkip("Not supported on this platform")
+        }
+        
         let app = XCUIApplication()
         app.deleteAndLaunch(withSpringboardAppName: "TestApp")
         
@@ -64,6 +61,7 @@ final class DeleteAppTests: XCTestCase {
         
         app.deleteAndLaunch(withSpringboardAppName: "TestApp")
     }
+    #endif
     
     
     @available(macOS, unavailable)
@@ -89,38 +87,4 @@ final class DeleteAppTests: XCTestCase {
         XCTAssert(app.staticTexts["No text set ..."].waitForExistence(timeout: 5))
         XCTAssert(app.staticTexts["No secure text set ..."].exists)
     }
-    
-    
-//    func testToggleStuff() throws {
-//        let app = XCUIApplication()
-//        app.launch()
-//        XCTAssert(app.wait(for: .runningForeground, timeout: 2))
-//        app.buttons["Toggles / Switches"].tap()
-//        
-//        XCTAssert(app.navigationBars["Toggles / Switches"].waitForExistence(timeout: 1))
-//        print(app.debugDescription)
-//        
-//        let toggle = app.switches["Selection"]
-//        
-//        func assertToggleState(_ expectedState: Bool, line: UInt = #line) {
-//            XCTAssert(app.staticTexts["Selection Value, \(expectedState)"].waitForExistence(timeout: 1))
-//            XCTAssertEqual(toggle.toggleState, expectedState)
-//        }
-//        
-//        assertToggleState(false)
-//        try toggle.flipToggle()
-//        assertToggleState(true)
-//        try toggle.setToggleState(isOn: true)
-//        assertToggleState(true)
-//        try toggle.setToggleState(isOn: true)
-//        assertToggleState(true)
-//        try toggle.setToggleState(isOn: false)
-//        assertToggleState(false)
-//        try toggle.flipToggle()
-//        assertToggleState(true)
-//        try toggle.flipToggle()
-//        assertToggleState(false)
-//        try toggle.setToggleState(isOn: true)
-//        assertToggleState(true)
-//    }
 }
